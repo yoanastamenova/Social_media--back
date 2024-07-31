d9d457cb83d9d35f4185635cd6a6d3ba505f3b51
Yoana <yoanabarbie@icloud.com>
2024-07-31 17:51:05 +0200

    feat: controllers started

--- src/database/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import 'dotenv/config';
-
-export const dbConnection = () => {
-    console.log('============');
-    console.log('Connected to MongoDB!');
-    return mongoose.connect(process.env.MONGO_URI, {})
-}
--- src/database/seeders/dbSeeder.js
+++
@@ -1,8 +0,0 @@
-import userSeeder from "./userSeeder.js";
-import postSeeder from "./postSeeder.js"
-
-(async () => { 
-    console.log("Starting seeders...")
-    await userSeeder();
-    await postSeeder();
-})();
