5b1b1bfeb2edd0dbb1b34cc286465e65deeffcd7
Yoana <yoanabarbie@icloud.com>
2024-08-04 14:40:34 +0200

    feat: Post model added

--- src/server.js
+++ src/server.js
@@ -11,6 +11,9 @@ import authRoutes from "./routes/auth.js"
 import { fileURLToPath } from 'url';
 import { register } from "./controllers/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+import { verifyToken } from './middleware/auth.js';
+import { createPost } from "./controllers/posts.js";
 
 
 /* APPLICATION & FILES SETTINGS */
@@ -43,10 +46,12 @@ const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register)
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
 // ROUTES
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes)
 
 /* MONGODB SETUP */
 
