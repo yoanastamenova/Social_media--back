69a2ec32ef49a241899c52173fa4d4dad6c8bc2e
Yoana <yoanabarbie@icloud.com>
2024-08-17 10:42:38 +0300

    fix: fixed seeder import as 1 file

--- data/dbSeeder.js
+++ data/dbSeeder.js
@@ -1,8 +1,35 @@
-import usersSeeder from "./usersSeeder.js";
-import postsSeeder from "./postsSeeder.js";
+import User from "../models/User.js"
+import Post from "../models/Post.js"
+import mongoose from "mongoose";
 
-(async () => { 
-    console.log("Starting seeders...")
-    await usersSeeder();
-    await postsSeeder();
-})();
+const seedUsers = async () => {
+  // Assuming you have a User model
+  await User.deleteMany({});
+  await User.insertMany(users);
+};
+
+const seedPosts = async () => {
+  // Assuming you have a Post model
+  await Post.deleteMany({});
+  await Post.insertMany(posts);
+};
+
+const seedDB = async () => {
+  await mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  
+  await seedUsers();
+  console.log('Users seeded successfully');
+  
+  await seedPosts();
+  console.log('Posts seeded successfully');
+  
+  mongoose.disconnect();
+};
+
+seedDB().catch(err => {
+  console.error('Failed seeding database', err);
+  mongoose.disconnect();
+});
--- server.js
+++ server.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from "./data/data.js";
 
 /* APPLICATION & FILES SETTINGS */
 
@@ -58,7 +61,11 @@ const PORT = process.env.PORT || 3001;
 mongoose
     .connect(process.env.MONGO_URI, {})
     .then(() => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-      })
-      .catch((error) => console.log(`${error} did not connect`));
+      app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  
+      /* ADD DATA ONE TIME */
+      // User.insertMany(users);
+      // Post.insertMany(posts);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
     
